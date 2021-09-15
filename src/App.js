d6e523e661e7dce35ee5aa747d551ce1b3ac991c
tomasVergara14 <tomasvergara272@gmail.com>
2021-09-15 16:42:44 -0300

    estructura carpetas

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
     </div>
   );
 }
