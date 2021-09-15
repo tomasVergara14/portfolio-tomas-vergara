9388200a7b3abe491a679e822ff81f66306b4466
tomasVergara14 <tomasvergara272@gmail.com>
2021-09-15 18:08:20 -0300

    view heigth

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Pages from '../Pages/Pages'
 
+import './Main.css'
+
 const Main = () => {
     return (
-        <div>
-            <Pages id="home" title="Hola" body="Wacho" />
-            <Pages id="about" title="Hola" body="Como estas" />
-            <Pages id="projects" title="Hola" body="Como estas" />
-            <Pages id="contact" title="Hola" body="Como estas" />
+        <div className="containerMain">
+            <Pages id="home" 
+            title="Hola" 
+            body="Wacho"/>
+            <Pages id="about" title="Hola" body="Lorem En este video crearemos una página de aterrizaje simple con un desplazamiento suave. Le mostraré 3 opciones que incluyen CSS puro (comportamiento de desplazamiento), jQuery y un script JS ligero" />
+            <Pages id="projects" title="Hola" body="Como En este video crearemos una página de aterrizaje simple con un desplazamiento suave. Le mostraré 3 opciones que incluyen CSS puro (comportamiento de desplazamiento), jQuery y un script JS ligero" />
+            <Pages id="contact" title="Hola" body="Como estas En este video crearemos una página de aterrizaje simple con un desplazamiento suave. Le mostraré 3 opciones que incluyen CSS puro (comportamiento de desplazamiento), jQuery y un script JS ligero" />
         </div>
     )
 }
--- src/Components/Pages/Pages.jsx
+++ src/Components/Pages/Pages.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import Title from '../Elements/Title/Title'
 import Body from '../Elements/Body/Body'
 
+import './Pages.css'
+
 const Pages = ({title,body}) => {
     return (
-        <div>
+        <div className="containerPages">
             <Title title={title}/>
             <Body body={body} />
         </div>
