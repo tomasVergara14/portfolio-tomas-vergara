7a9ddaacd1857d658b9b9124e436c7bd2eafa12a
tomasVergara14 <tomasvergara272@gmail.com>
2021-09-17 19:36:43 -0300

    about me text

--- src/Components/Pages/About/About.jsx
+++ src/Components/Pages/About/About.jsx
@@ -10,7 +10,6 @@ const About = () => {
             <div className="aboutTitle">
                 <AboutMeIcon/>
             </div>
-            <p className="bodyAbout">My name is Tomas, I am 25 years old and I live in Cordoba.</p>
             <p> An organized person who enjoys learning new skills, web development, sports and cooking. </p>
         </div>
         
