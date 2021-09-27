9808888b66ff0a8f4868b2051c1204de61a8536e
tomasVergara14 <tomasvergara272@gmail.com>
2021-09-27 19:02:33 -0300

    agregando projectos

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -2,6 +2,8 @@ import portfolioImage from './Portfolio.png'
 import toDoImage from './toDo.png'
 import coffeShopImage from './coffeShop.png'
 import ApiCountriesImage from './ApiCountries.png'
+import drumsImage from './drums.png'
+import reactClock from './reactClock.png'
 
 
 export const projectData = [
@@ -33,4 +35,19 @@ export const projectData = [
         git:'https://github.com/tomasVergara14/proyecto-coffe',
         link:'http://to-do-express.herokuapp.com/',
     },
+    {
+        name:'Drums',
+        image:drumsImage,
+        alt:'image-portfolio',
+        git:'https://github.com/tomasVergara14/drums',
+        link:'https://vanilla-drums.herokuapp.com/',
+    },
+    {
+        name:'React Clock',
+        image:reactClock,
+        alt:'image-portfolio',
+        git:'https://github.com/tomasVergara14/react-clock',
+        link:'https://react-clock-chronometer.netlify.app/',
+    }
+
 ]
